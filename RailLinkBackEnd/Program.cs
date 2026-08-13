51e168435045717333ac5c3d02c8ca447b92dc45
maeji <angel97102106@gmail.com>
2026-08-13 09:25:53 +0900

    fix :

--- RailLinkBackEnd/Program.cs
+++ RailLinkBackEnd/Program.cs
@@ -9,12 +9,10 @@ builder.Services.AddOpenApi();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
     app.MapOpenApi();
-}
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
